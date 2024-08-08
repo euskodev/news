b8cadd3e1dd77e594232ccad18be406bf415c865
Iñaki Retegi <retegi84@gmail.com>
2024-08-08 18:45:22 +0200

    cambios en news

--- applications/home/admin.py
+++ applications/home/admin.py
@@ -4,7 +4,7 @@ from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ("date_time", "title", "image", "author", "content", "category","highlight1","highlight2","highlight2","recent_post1","recent_post2","recent_post3")
+    list_display = ("date_time", "title", "image", "author", "category","highlight1","highlight2","highlight2","recent_post1","recent_post2","recent_post3")
 admin.site.register(News,NewsAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
--- applications/home/urls.py
+++ applications/home/urls.py
@@ -7,4 +7,6 @@ app_name = 'home_app'
 urlpatterns = [
     path('', views.HomePageView.as_view(),name='home',),
     #path('news_detail', views.HomePageView.as_view(),name='home',),
+    path('home/articulo/<int:pk>/', views.NewsDetailView.as_view(), name='news-detail' 
+),
 ]
