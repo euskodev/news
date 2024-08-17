20d98bff6f1031c273eb5db9ad8f17df551eccf3
olvin78 <olvind78@gmail.com>
2024-08-17 16:35:50 +0200

    prueba

--- news/urls.py
+++ news/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path,include
 from applications import home
-
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applications.home.urls')),
-
+    path("ads.txt",
+         RedirectView.as_view(url=staticfiles_storage.url("ads.txt")),),
 ]
+
 
 
 
